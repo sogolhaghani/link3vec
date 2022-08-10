81892ed40673b302f82bf9aa48503f12faf483fa
sogolhaghani <haghani.sogol@gmail.com>
2022-08-10 19:03:15 +0430

    eval

--- src/model.py
+++ src/model.py
@@ -4,39 +4,42 @@ import tensorflow as tf
 
 class Link3Vec():
     def __init__(self,config):
-        self.train = config.train
-        self.test = config.test
-        self.validation = config.validation
-        self.relations = config.relations
-        self.entities = config.entities
+
+        self.dim = config['dim']
+        self.kns = config['kns']
+        self.kns_r = config['kns_r']
+        self.alpha = config['alpha']
+        self.beta = config['beta']
 
         z = tf.constant(5, dtype = tf.float64)
-        self.nn1 = tf.random.uniform( shape=(self.entities.shape[0], self.dim),maxval=tf.math.truediv(z , tf.cast(self.dim,tf.float64),'maxval'), dtype=tf.dtypes.float64, seed=1, name='nn1')
-        self.nn0 =tf.random.uniform(shape=(self.entities.shape[0], self.dim), maxval=tf.math.truediv(z , tf.cast(self.dim,tf.float64),'maxval'), dtype=tf.dtypes.float64, seed=1, name='nn0')
-        self.nn2 = tf.random.uniform(shape=(self.relations.shape[0], self.dim), maxval=tf.math.truediv(z , tf.cast(self.dim,tf.float64),'maxval'), dtype=tf.dtypes.float64, seed=1, name='nn2')     
+        self.nn1 = tf.random.uniform( shape=(config['entities'].shape[0], self.dim),maxval=tf.math.truediv(z , tf.cast(self.dim,tf.float64),'maxval'), dtype=tf.dtypes.float64, seed=1, name='nn1')
+        self.nn0 =tf.random.uniform(shape=(config['entities'].shape[0], self.dim), maxval=tf.math.truediv(z , tf.cast(self.dim,tf.float64),'maxval'), dtype=tf.dtypes.float64, seed=1, name='nn0')
+        self.nn2 = tf.random.uniform(shape=(config['relations'].shape[0], self.dim), maxval=tf.math.truediv(z , tf.cast(self.dim,tf.float64),'maxval'), dtype=tf.dtypes.float64, seed=1, name='nn2')     
         self.startIndex=  tf.constant(0)
-        if config.read_Last_state == True:
-            self.nn0 = tf.convert_to_tensor( np.load(os.path.join(config.path, "nn0.npy") ).astype(dtype=np.float64), dtype=tf.float64)
-            self.nn1 = tf.convert_to_tensor( np.load(os.path.join(config.path, "nn1.npy") ).astype(dtype=np.float64), dtype=tf.float64)
-            self.nn2 = tf.convert_to_tensor( np.load(os.path.join(config.path, "nn2.npy") ).astype(dtype=np.float64), dtype=tf.float64)
-            self.startIndex= np.load(os.path.join(config.path, "x.npy"))
-        with tf.device('/cpu:0'):
-            self.nSam_ent = tf.gather( tf.random.categorical([tf.gather(self.entities, 2, axis=1)], tf.math.multiply(self.kns,self.train.shape[0]) , dtype=None, seed=None),0)
-            self.nSam_rel  = tf.gather( tf.random.categorical([tf.gather(self.relations, 2, axis=1)], tf.math.multiply(self.kns_r,self.train.shape[0]) , dtype=None, seed=None),0)
 
-    def _score_and_update(self, triple, tIndex):
+        self.nSam_ent = config['nSam_ent']
+        self.nSam_rel  = config['nSam_rel']
+        self._save_embedings = config['_save_embedings']
+        if config['read_Last_state'] == True:
+            self.nn0 = tf.convert_to_tensor( np.load(os.path.join(config['path'], "nn0.npy") ).astype(dtype=np.float64), dtype=tf.float64)
+            self.nn1 = tf.convert_to_tensor( np.load(os.path.join(config['path'], "nn1.npy") ).astype(dtype=np.float64), dtype=tf.float64)
+            self.nn2 = tf.convert_to_tensor( np.load(os.path.join(config['path'], "nn2.npy") ).astype(dtype=np.float64), dtype=tf.float64)
+            self.startIndex= np.load(os.path.join(config['path'], "x.npy"))
+
+    def _score_and_update_tail(self, triple, tIndex):
         paddings = tf.constant([[0, 1,], [0, 0]])
         _head_index = tf.gather(triple,0)
         _relation_index = tf.gather(triple,2)
-        _vHead = tf.gather(nn0,_head_index)
-        _vRel = tf.gather(nn2,_relation_index)
+        _vHead = tf.gather(self.nn0,_head_index)
+        _vRel = tf.gather(self.nn2,_relation_index)
 
         samples = [tf.slice(self.nSam_ent,begin=[tf.math.multiply(tIndex, self.kns)],size=[self.kns])]
         samples = tf.pad(samples, paddings, constant_values=0)
         samples =  tf.cast(samples, tf.int64)         
         samples = tf.transpose(tf.concat([samples, [[tf.gather(triple,1)],[1]]], 1))
         indices = tf.gather(samples, 0, axis=1)
-        _nn1_samples = tf.gather(nn1, indices)
+        _nn1_samples = tf.gather(self.nn1, indices)
+
         _sigmoid =tf.math.sigmoid(tf.tensordot( _nn1_samples , tf.transpose(tf.math.add(_vHead , _vRel)) , axes=1))
         cost = tf.math.subtract(tf.cast(tf.gather(samples, 1, axis=1), tf.float64) , _sigmoid)
         g = tf.math.multiply(self.alpha , cost)
@@ -44,19 +47,95 @@ class Link3Vec():
 
         # error in first layer
         _nn1_samples_err = tf.math.add(tf.math.multiply(tf.math.add(_vRel,_vHead)  , tf.reshape(g, (tf.math.add(self.kns,1), 1))) , _nn1_samples)
-        nn1 = tf.tensor_scatter_nd_update(nn1,tf.expand_dims(indices, 1),_nn1_samples_err)
+        self.nn1 = tf.tensor_scatter_nd_update(self.nn1,tf.expand_dims(indices, 1),_nn1_samples_err)
       
         # error second layer nn2
         err_nn2 = tf.math.multiply( _nn1_samples, tf.reshape(g1, (g1.shape[0], 1)))
         _nn2_sample = tf.math.add(tf.math.reduce_sum(err_nn2, axis=0, keepdims=False), _vRel)
         indices = tf.constant(_relation_index)
-        nn2 = tf.tensor_scatter_nd_update(nn2,tf.expand_dims([indices], 1), tf.reshape(_nn2_sample,(1, _nn2_sample.shape[0])))
+        self.nn2 = tf.tensor_scatter_nd_update(self.nn2,tf.expand_dims([indices], 1), tf.reshape(_nn2_sample,(1, _nn2_sample.shape[0])))
 
         err_nn0 = tf.math.multiply( _nn1_samples, tf.reshape(g, (g.shape[0], 1)))
         _nn0_sample = tf.math.add(tf.math.reduce_sum(err_nn0, axis=0, keepdims=False),_vHead)
         indices = tf.constant(_head_index)
-        nn0 = tf.tensor_scatter_nd_update(nn0,tf.expand_dims([indices], 1), tf.reshape(_nn0_sample,(1, _nn0_sample.shape[0])))
+        self.nn0 = tf.tensor_scatter_nd_update(self.nn0,tf.expand_dims([indices], 1), tf.reshape(_nn0_sample,(1, _nn0_sample.shape[0])))
+        return tf.gather(_sigmoid,self.kns)
 
+    def _score_and_update_head(self, triple, tIndex):      
+        paddings = tf.constant([[0, 1,], [0, 0]])  
+        _tail_index = tf.gather(triple,1)
+        _relation_index = tf.gather(triple,2)
+        
+        _vTail = tf.gather(self.nn1,_tail_index)
+        _vRel = tf.gather(self.nn2,_relation_index)
+
+        samples = [tf.slice(self.nSam_ent,begin=[tf.math.multiply(tIndex, self.kns)],size=[self.kns])]
+        samples = tf.pad(samples, paddings, constant_values=0)
+        samples =  tf.cast(samples, tf.int64)         
+        samples = tf.transpose(tf.concat([samples, [[tf.gather(triple,0)],[1]]], 1))
+        indices = tf.gather(samples, 0, axis=1)
+        _nn0_samples = tf.gather(self.nn0, indices)
+
+
+        _sigmoid =tf.math.sigmoid(tf.tensordot( _nn0_samples , tf.transpose(tf.math.add(_vTail , _vRel)) , axes=1))
+        cost = tf.math.subtract(tf.cast(tf.gather(samples, 1, axis=1), tf.float64) , _sigmoid)
+        g = tf.math.multiply(self.alpha , cost)
+        g1 = tf.math.multiply(self.beta , cost)
+ 
+        _nn0_samples_err = tf.math.add(tf.math.multiply(tf.math.add(_vTail , _vRel) , tf.reshape(g, (tf.math.add(self.kns,1), 1))) , _nn0_samples)
+        self.nn0 = tf.tensor_scatter_nd_update(self.nn0,tf.expand_dims(indices, 1),_nn0_samples_err)
+
+        # error second layer nn2
+        err_nn2 = tf.math.multiply( _nn0_samples, tf.reshape(g1, (g1.shape[0], 1)))
+        _nn2_sample = tf.math.add(tf.math.reduce_sum(err_nn2, axis=0, keepdims=False), _vRel)
+        indices = tf.constant(_relation_index)
+        self.nn2 = tf.tensor_scatter_nd_update(self.nn2,tf.expand_dims([indices], 1), tf.reshape(_nn2_sample,(1, _nn2_sample.shape[0])))
+
+        # error second layer nn2
+        _err_nn1 = tf.math.multiply( _nn0_samples, tf.reshape(g, (g.shape[0], 1)))
+        _nn1_sample = tf.math.add(tf.math.reduce_sum(_err_nn1, axis=0, keepdims=False), _vTail)
+        indices = tf.constant(_tail_index)
+        self.nn1 = tf.tensor_scatter_nd_update(self.nn1,tf.expand_dims([indices], 1), tf.reshape(_nn1_sample,(1, _nn1_sample.shape[0])))
+        return tf.gather(_sigmoid,self.kns)
+
+    def _score_and_update_rel(self, triple, tIndex):
+        paddings = tf.constant([[0, 1,], [0, 0]])  
+        _head_index = tf.gather(triple,0)
+        _tail_index = tf.gather(triple,1)
+        _relation_index = tf.gather(triple,2)
+        
+        _vHead = tf.gather(self.nn0,_head_index)
+        _vTail = tf.gather(self.nn1,_tail_index)
+
+        samples = [tf.slice(self.nSam_rel,begin=[tf.math.multiply(tIndex, self.kns_r)],size=[self.kns_r])]
+        samples = tf.pad(samples, paddings, constant_values=0)
+        samples =  tf.cast(samples, tf.int64)         
+        samples = tf.transpose(tf.concat([samples, [[_relation_index],[1]]], 1))
+        indices = tf.gather(samples, 0, axis=1)
+        _nn2_samples = tf.gather(self.nn2, indices)
+
+        _sigmoid =tf.math.sigmoid(tf.tensordot( _nn2_samples , tf.transpose(tf.math.add(_vTail , _vHead)) , axes=1))
+        cost = tf.math.subtract(tf.cast(tf.gather(samples, 1, axis=1), tf.float64) , _sigmoid)
+        g = tf.math.multiply(self.alpha , cost)
+        g1 = tf.math.multiply(self.beta , cost)
+        
+        # error in first layer
+        _nn2_samples_err = tf.math.add(tf.math.multiply(tf.math.add(_vTail , _vHead) , tf.reshape(g1, (tf.math.add(self.kns_r,1), 1))) , _nn2_samples)
+        self.nn2 = tf.tensor_scatter_nd_update(self.nn2,tf.expand_dims(indices, 1),_nn2_samples_err)
+        
+        # error second layer nn2
+        _err_nn1 = tf.math.multiply( _nn2_samples, tf.reshape(g, (g.shape[0], 1)))
+        _nn1_sample = tf.math.add(tf.math.reduce_sum(_err_nn1, axis=0, keepdims=False), _vTail)
+        indices = tf.constant(_tail_index)
+        self.nn1 = tf.tensor_scatter_nd_update(self.nn1,tf.expand_dims([indices], 1), tf.reshape(_nn1_sample,(1, _nn1_sample.shape[0])))
+
+        err_nn0 = tf.math.multiply( _nn2_samples, tf.reshape(g, (g.shape[0], 1)))
+        _nn0_sample = tf.math.add(tf.math.reduce_sum(err_nn0, axis=0, keepdims=False),_vHead)
+        indices = tf.constant(_head_index)
+        self.nn0 = tf.tensor_scatter_nd_update(self.nn0,tf.expand_dims([indices], 1), tf.reshape(_nn0_sample,(1, _nn0_sample.shape[0])))  
+        return tf.gather(_sigmoid,self.kns_r)      
+
+    # TODO : save at step
     def _save(self, step):        
         if self._save_embedings:
             np.save(os.path.join(self.path, "nn0.npy"), self.nn0.numpy())
--- src/run.py
+++ src/run.py
@@ -6,32 +6,43 @@ from model import Link3Vec
 
 def train(config):
     model = Link3Vec(config=config)
-    for x in range(config.startIndex , config.iteration):
-        train = tf.random.shuffle(config.train, seed=None, name=None)
-        for tIndex in range(0 , train.shape[0]):
+    for x in range(model.startIndex , config['iteration']):
+        train = tf.random.shuffle(config['train'], seed=None, name=None)
+        tensor = []
+        _tSize = 5000#train.shape[0]
+        for tIndex in range(0 , _tSize):
             triple = tf.gather(train, tIndex)
-            model._score_and_update(triple, tIndex)
+            # if x%3 ==0 :
+            tensor.append((model._score_and_update_tail(triple, tIndex) + model._score_and_update_rel(triple, tIndex) + model._score_and_update_head(triple, tIndex) ) / 3 )      
             
         if x > 20 and x % 5 == 0:
-            model._save(x) 
+            model._save(x)
+        if x % 3 ==0:
+            print(sum(tensor) / _tSize)
 
 def run():
     config = {}
     
-    config.path = '../data/WN18_numpy'
-    config.train = tf.convert_to_tensor( np.load(os.path.join(config.path, "train.npy") ).astype(dtype=np.int64), dtype=tf.int64)
-    config.validation = tf.convert_to_tensor( np.load(os.path.join(config.path, "validation.npy") ).astype(dtype=np.int64), dtype=tf.int64)
-    config.test = tf.convert_to_tensor( np.load(os.path.join(config.path, "test.npy") ).astype(dtype=np.int64), dtype=tf.int64)
-    config.relations = tf.convert_to_tensor( np.load(os.path.join(config.path, "relations.npy") ).astype(dtype=np.float64), dtype=tf.float64)
-    config.entities = tf.convert_to_tensor( np.load(os.path.join(config.path, "entities.npy") ).astype(dtype=np.float64), dtype=tf.float64)
+    config['path'] = '../data/WN18_numpy/'
+    config['train'] = tf.convert_to_tensor( np.load(os.path.join(config['path'], "train.npy") ).astype(dtype=np.int64), dtype=tf.int64)
+    config['validation'] = tf.convert_to_tensor( np.load(os.path.join(config['path'], "validation.npy") ).astype(dtype=np.int64), dtype=tf.int64)
+    config['test'] = tf.convert_to_tensor( np.load(os.path.join(config['path'], "test.npy") ).astype(dtype=np.int64), dtype=tf.int64)
+    config['relations'] = tf.convert_to_tensor( np.load(os.path.join(config['path'], "relations.npy") ).astype(dtype=np.float64), dtype=tf.float64)
+    config['entities'] = tf.convert_to_tensor( np.load(os.path.join(config['path'], "entities.npy") ).astype(dtype=np.float64), dtype=tf.float64)
+    config['_save_embedings'] = False
+    config['read_Last_state'] = False
+    config['iteration'] = tf.constant(2000)
+    config['dim'] = tf.constant(100)
+    config['kns'] = tf.constant(1)
+    config['kns_r'] = tf.constant(1)
+    config['alpha'] = tf.Variable(0.07, dtype = tf.float64)
+    config['beta'] = tf.Variable(0.007, dtype = tf.float64)
 
-    config.save_embedings = False
-    config.read_Last_state = False
-    config.iteration = tf.constant(2000)
-    config.dim = tf.constant(100)
-    config.kns = tf.constant(1)
-    config.kns_r = tf.constant(1)
-    config.alpha = tf.Variable(0.07, dtype = tf.float64)
-    config.beta = tf.Variable(0.007, dtype = tf.float64)
+    with tf.device('/cpu:0'):
+        config['nSam_ent'] = tf.gather( tf.random.categorical([tf.gather(config['entities'], 2, axis=1)], tf.math.multiply(config['kns'],config['train'].shape[0]) , dtype=None, seed=None),0)
+        config['nSam_rel'] = tf.gather( tf.random.categorical([tf.gather(config['relations'], 2, axis=1)], tf.math.multiply(config['kns_r'],config['train'].shape[0]) , dtype=None, seed=None),0)
 
     train(config)
+
+if __name__ == "__main__":
+    run()    
